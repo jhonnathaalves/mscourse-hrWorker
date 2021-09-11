55905345d0c02485fdfbd18159e59ba99740abad
unknown <jhonnatha16011987@yahoo.com.br>
2021-09-10 23:39:06 -0300

    Publicando o projeto em container

--- hr-worker/src/main/java/com/jhonnatha/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/jhonnatha/hrworker/resources/WorkerResource.java
@@ -35,7 +35,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
@@ -48,13 +48,13 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById( @PathVariable Long id ){
 		
-		
+		/*
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+		*/
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
